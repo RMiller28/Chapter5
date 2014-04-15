c1c9dcbd6915ef4127f8e50bf8c424428f4068b4
RMiller28 <Millerrycki5@gmail.com>
2014-04-15 10:10:03 -0400

    Finish

---
+++ ChatAWhile.cs/ChatAWhile.cs
@@ -0,0 +1,38 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 11.00
+# Visual C# Express 2010
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ChatAWhile.cs", "ChatAWhile.cs.csproj", "{34A6FDFB-0637-4BE0-8D79-71F9D54D1BE9}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "CheckZips.cs", "..\CheckZips.cs\CheckZips.cs.csproj", "{7031A28D-538B-4311-B2E8-7C2B26B5DC8E}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "DeliveryCharges.cs", "..\DeliveryCharges.cs\DeliveryCharges.cs.csproj", "{B2BA1DD4-304A-444C-BFCB-038FE67249E9}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "IntegerList.cs", "..\IntegerList.cs\IntegerList.cs\IntegerList.cs.csproj", "{D5D3D716-B2EB-4818-9758-6A667ED52779}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x86 = Debug|x86
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{34A6FDFB-0637-4BE0-8D79-71F9D54D1BE9}.Debug|x86.ActiveCfg = Debug|x86
+		{34A6FDFB-0637-4BE0-8D79-71F9D54D1BE9}.Debug|x86.Build.0 = Debug|x86
+		{34A6FDFB-0637-4BE0-8D79-71F9D54D1BE9}.Release|x86.ActiveCfg = Release|x86
+		{34A6FDFB-0637-4BE0-8D79-71F9D54D1BE9}.Release|x86.Build.0 = Release|x86
+		{7031A28D-538B-4311-B2E8-7C2B26B5DC8E}.Debug|x86.ActiveCfg = Debug|x86
+		{7031A28D-538B-4311-B2E8-7C2B26B5DC8E}.Debug|x86.Build.0 = Debug|x86
+		{7031A28D-538B-4311-B2E8-7C2B26B5DC8E}.Release|x86.ActiveCfg = Release|x86
+		{7031A28D-538B-4311-B2E8-7C2B26B5DC8E}.Release|x86.Build.0 = Release|x86
+		{B2BA1DD4-304A-444C-BFCB-038FE67249E9}.Debug|x86.ActiveCfg = Debug|x86
+		{B2BA1DD4-304A-444C-BFCB-038FE67249E9}.Debug|x86.Build.0 = Debug|x86
+		{B2BA1DD4-304A-444C-BFCB-038FE67249E9}.Release|x86.ActiveCfg = Release|x86
+		{B2BA1DD4-304A-444C-BFCB-038FE67249E9}.Release|x86.Build.0 = Release|x86
+		{D5D3D716-B2EB-4818-9758-6A667ED52779}.Debug|x86.ActiveCfg = Debug|x86
+		{D5D3D716-B2EB-4818-9758-6A667ED52779}.Debug|x86.Build.0 = Debug|x86
+		{D5D3D716-B2EB-4818-9758-6A667ED52779}.Release|x86.ActiveCfg = Release|x86
+		{D5D3D716-B2EB-4818-9758-6A667ED52779}.Release|x86.Build.0 = Release|x86
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
